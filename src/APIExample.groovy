ebbfd6a7c449d0605eed39e936e7823ec94d4110
Hycel Taylor <hycel1@gmail.com>
2017-04-04 13:27:14 -0700

    Works!

--- src/APIExample.groovy
+++ src/APIExample.groovy
@@ -41,6 +41,17 @@ class Simulation {
     }
   }
 
+  def getAverageBalance(String accountType) {
+    def sql = getConnection()
+    def query = """select round(avg(aco.balance)) as avg from account aco
+                   join account_type act on act.id = aco.account_type_id
+                   where act.name = '${accountType}'"""
+
+    def row = sql.rows(query.toString())
+
+    return row[0]['avg']
+  }
+
   def loadTestData() {
     def scenarios = [
       'AccountTypeRestScenario',
@@ -65,11 +76,6 @@ class Simulation {
       api.domainSetLoopCount('CardPool', cardPoolCount.toString())
       api.scenarioRun()
 
-      api.scenarioLoad("${scenarioPath}OpenAccountRestScenario.grs")
-      api.domainSetLoopCount('OpenAccount', customerCount.toString())
-      api.generatorParameterSet('OpenAccount.branchCode', 0, 'var2', (branchCount - 1).toString())
-      api.scenarioRun()
-
       println('Load Data All Done!')
     } catch (GenRocketException e) {
       println(e.getMessage())
@@ -81,24 +87,64 @@ class Simulation {
     loadTestData()
 
     EngineAPI api = new EngineManual()
+    Boolean done = false
+    Float avg = 0
 
-    try {
-      api.scenarioLoad("${scenarioPath}AccountWithdrawalRestScenario.grs")
-      api.domainSetLoopCount('WithdrawalDays', 1.toString())
-      api.domainSetLoopCount('User', customerCount.toString())
-      api.scenarioRun()
+    // Open all checking accounts with a balance of 1000.00
+    // Open all savings accounts with a balance of 3000.00
+    api.scenarioLoad("${scenarioPath}OpenAccountRestScenario.grs")
+    api.domainSetLoopCount('OpenAccount', customerCount.toString())
+    api.generatorParameterSet('OpenAccount.branchCode', 0, 'var2', (branchCount - 1).toString())
+    api.generatorParameterSet('OpenAccount.checking', 0, 'map', [Silver: '1000.00', Gold: '1000.00', Platinum: '1000.00'])
+    api.generatorParameterSet('OpenAccount.savings', 0, 'map', [Silver: '3000.00', Gold: '3000.00', Platinum: '3000.00'])
+    api.scenarioRun()
 
-      api.scenarioLoad("${scenarioPath}AccountDepositRestScenario.grs")
-      api.domainSetLoopCount('User', customerCount.toString())
-      api.scenarioRun()
+    while (!done) {
+      try {
+        // Withdrawal 100.00, 150.00, 200.00 or 250.00 from checking
+        api.scenarioLoad("${scenarioPath}AccountWithdrawalRestScenario.grs")
+        api.domainSetLoopCount('WithdrawalDays', 1.toString())
+        api.generatorParameterSet('User.amount', 0, 'list', ['100','150','200','250'])
+        api.domainSetLoopCount('User', customerCount.toString())
+        api.scenarioRun()
 
-      api.scenarioLoad("${scenarioPath}AccountTransferRestScenario.grs")
-      api.domainSetLoopCount('User', customerCount.toString())
-      api.scenarioRun()
+        avg = getAverageBalance('Checking')
 
-      println('Simulation All Done!')
-    } catch (GenRocketException e) {
-      println(e.getMessage())
+        if (avg <= 500) {
+          // Switch fromCardNumber to Savings and toCardNumber to Checking
+          // Transfer 750 from savings into checking
+          api.scenarioLoad("${scenarioPath}AccountTransferRestScenario.grs")
+          api.generatorParameterSet('User.fromCardNumber', 0, 'equation', 'var1 + var1')
+          api.generatorParameterSet('User.toCardNumber', 0, 'equation', 'var1 + var1 - 1')
+          api.generatorParameterSet('User.amount', 0, 'value', 750.00.toString())
+          api.domainSetLoopCount('User', customerCount.toString())
+          api.scenarioRun()
+        }
+
+        avg = getAverageBalance('Savings')
+
+        if (avg <= 1500) {
+          // Deposit 2000.00 into checking
+          api.scenarioLoad("${scenarioPath}AccountDepositRestScenario.grs")
+          api.domainSetLoopCount('User', customerCount.toString())
+          api.generatorParameterSet('User.amount', 0, 'value', 2000.00.toString())
+          api.scenarioRun()
+
+          // Transfer 1500.00 from checking to savings
+          api.scenarioLoad("${scenarioPath}AccountTransferRestScenario.grs")
+          api.generatorParameterSet('User.amount', 0, 'value', 1500.00.toString())
+          api.domainSetLoopCount('User', customerCount.toString())
+          api.scenarioRun()
+
+          // End the simulation
+          done = true
+        }
+
+      } catch (GenRocketException e) {
+        println(e.getMessage())
+      }
     }
+
+    println('Simulation All Done!')
   }
 }
