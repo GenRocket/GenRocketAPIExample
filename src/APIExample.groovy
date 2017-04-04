0472b6511bdd9f0a318b8559ab13bf8d3a54f81a
Hycel Taylor <hycel1@gmail.com>
2017-04-03 22:05:19 -0700

    Updates

--- src/APIExample.groovy
+++ src/APIExample.groovy
@@ -13,86 +13,87 @@ import com.genRocket.engine.EngineAPI
 import com.genRocket.engine.EngineManual
 import groovy.sql.Sql
 
-//emptyTables()
-//loadTestData()
-simulation()
+new Simulation().simulationOne()
 
-def getConnection() {
-  return Sql.newInstance("jdbc:mysql://localhost:3306/genrocket_bank", "root", "admin", "com.mysql.jdbc.Driver")
-}
-
-def emptyTables() {
-  def sql = getConnection()
-  def tables = [
-    'card', 'card_type', 'customer', 'customer_level', 'transaction',
-    'transaction_type', 'account', 'account_type', 'branch', 'user', 'card_pool'
-  ]
-
-  tables.each { table ->
-    println "delete from ${table}..."
-    sql.execute("delete from ${table}".toString())
-  }
-}
-
-def loadTestData() {
+class Simulation {
   def fileSep = System.getProperty("file.separator")
   def userHome = System.getProperty("user.home")
   def scenarioPath = "${userHome}${fileSep}Downloads${fileSep}"
-  def scenarios = [
-    'AccountTypeRestScenario',
-    'CardTypeRestScenario',
-    'CustomerLevelRestScenario',
-    'TransactionTypeRestScenario',
-  ]
 
-  EngineAPI api = new EngineManual()
+  def branchCount = 1000
+  def cardPoolCount = 40000
+  def customerCount = 10000
 
-  try {
-    scenarios.each { scenario ->
-      api.scenarioLoad("${scenarioPath}${scenario}.grs")
-      api.scenarioRun()
+  def getConnection() {
+    return Sql.newInstance("jdbc:mysql://localhost:3306/genrocket_bank", "root", "admin", "com.mysql.jdbc.Driver")
+  }
+
+  def emptyTables() {
+    def sql = getConnection()
+    def tables = [
+      'card', 'card_type', 'customer', 'customer_level', 'transaction',
+      'transaction_type', 'account', 'account_type', 'branch', 'user', 'card_pool'
+    ]
+
+    tables.each { table ->
+      println "delete from ${table}..."
+      sql.execute("delete from ${table}".toString())
     }
-
-    def branchCount = 1000
-    def cardPoolCount = 40000
-    def customerCount = 10000
-
-    api.scenarioLoad("${scenarioPath}BranchRestScenario.grs")
-    api.domainSetLoopCount('Branch', branchCount.toString())
-    api.scenarioRun()
-
-    api.scenarioLoad("${scenarioPath}CardPoolRestScenario.grs")
-    api.domainSetLoopCount('CardPool', cardPoolCount.toString())
-    api.scenarioRun()
-
-    api.scenarioLoad("${scenarioPath}OpenAccountRestScenario.grs")
-    api.domainSetLoopCount('OpenAccount', customerCount.toString())
-    api.generatorParameterSet('OpenAccount.branchCode', 0, 'var2', (branchCount - 1).toString())
-    api.scenarioRun()
-
-    api.scenarioLoad("${scenarioPath}AccountWithdrawalRestScenario.grs")
-    api.scenarioRun()
-
-    println('Load Data All Done!')
-  } catch (GenRocketException e) {
-    println(e.getMessage())
   }
-}
 
-def simulation() {
-  def fileSep = System.getProperty("file.separator")
-  def userHome = System.getProperty("user.home")
-  def scenarioPath = "${userHome}${fileSep}Downloads${fileSep}"
+  def loadTestData() {
+    def fileSep = System.getProperty("file.separator")
+    def userHome = System.getProperty("user.home")
+    def scenarioPath = "${userHome}${fileSep}Downloads${fileSep}"
+    def scenarios = [
+      'AccountTypeRestScenario',
+      'CardTypeRestScenario',
+      'CustomerLevelRestScenario',
+      'TransactionTypeRestScenario',
+    ]
 
-  EngineAPI api = new EngineManual()
+    EngineAPI api = new EngineManual()
 
-  try {
+    try {
+      scenarios.each { scenario ->
+        api.scenarioLoad("${scenarioPath}${scenario}.grs")
+        api.scenarioRun()
+      }
 
-    api.scenarioLoad("${scenarioPath}AccountWithdrawalRestScenario.grs")
-    api.scenarioRun()
+      api.scenarioLoad("${scenarioPath}BranchRestScenario.grs")
+      api.domainSetLoopCount('Branch', branchCount.toString())
+      api.scenarioRun()
 
-    println('Simulation All Done!')
-  } catch (GenRocketException e) {
-    println(e.getMessage())
+      api.scenarioLoad("${scenarioPath}CardPoolRestScenario.grs")
+      api.domainSetLoopCount('CardPool', cardPoolCount.toString())
+      api.scenarioRun()
+
+      api.scenarioLoad("${scenarioPath}OpenAccountRestScenario.grs")
+      api.domainSetLoopCount('OpenAccount', customerCount.toString())
+      api.generatorParameterSet('OpenAccount.branchCode', 0, 'var2', (branchCount - 1).toString())
+      api.scenarioRun()
+
+      println('Load Data All Done!')
+    } catch (GenRocketException e) {
+      println(e.getMessage())
+    }
+  }
+
+  public simulationOne() {
+    emptyTables()
+    loadTestData()
+
+    EngineAPI api = new EngineManual()
+
+    try {
+      api.scenarioLoad("${scenarioPath}AccountWithdrawalRestScenario.grs")
+      api.domainSetLoopCount('DepositDays', 1.toString())
+      api.domainSetLoopCount('User', customerCount.toString())
+      api.scenarioRun()
+
+      println('Simulation All Done!')
+    } catch (GenRocketException e) {
+      println(e.getMessage())
+    }
   }
 }
